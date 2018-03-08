e1f8b3a2bb83e69a074868440e27340eca897268
Susie Chac <susiechac@campus-013-006.ucdavis.edu>
2018-03-07 16:47:44 -0800

    got column name

--- maxTweeter.c
+++ maxTweeter.c
@@ -1,7 +1,54 @@
 #include <stdlib.h>
 #include <stdio.h>
+#include <string.h>
 
-int main(int argc, char** argcv)
+int getColNum(char* line, char* str)
 {
-	printf("hello");
+	const char* tok;
+	int col = 0;
+	for(tok = strtok(line, ",");  ; tok = strtok(NULL, ",\n"))
+	{
+		printf("tok: %s\n", tok);
+		if(strcmp(tok, str) == 0) {
+			printf("str: %s\n", str);
+			return col;
+		}
+		col++;
+		printf("colnum: %d\n", col);
+	}
+	return -1;
+}
+
+const char* getField(char* line, int num)
+{
+	const char* tok;
+	for(tok = strtok(line, ",");  ; tok = strtok(NULL, ",\n"))
+	{
+		printf("field\n");
+		if(!--num)
+			return tok;
+	}
+	return NULL;
+}
+
+int main(int argc, char** argv)
+{
+	printf("Input file: %s\n", argv[1]);
+	FILE* stream = fopen(argv[1], "r");
+
+	char colNames[1024];
+	fgets(colNames, 1024, stream);
+	int nameCol = getColNum(colNames, "name");
+
+	char line[1024];
+
+
+	while(fgets(line, 1024, stream))
+	{
+		char* tmp = strdup(line);
+
+		const char* out = getField(tmp, nameCol);
+		printf("Name: %s\n", out);
+		break;
+	}
 }
